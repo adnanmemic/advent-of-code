c4ee5bd9facdd48379322ea014554a48e68c3c8a
Adnan Memic <adnan.memic27@gmail.com>
2026-09-21 16:58:24 +0200

    feat(2025/day02): add main

--- 2025/day02/Main.java
+++ 2025/day02/Main.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String path = "resources/input.txt";
+        String[] idRanges = null; 
+
+        FileHandler file = new FileHandler(path);
+        try {
+            idRanges = file.readFile();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        Id id = new Id(idRanges);
+        List<String> invalidIds = id.invalidIds();
+        long sum = id.sumOfInvalidIds(invalidIds);
+
+        System.out.println(sum);
     }
 }
