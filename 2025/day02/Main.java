34cc0686b7b4986a60aa9ceef0dd63ebc03fe48e
Adnan Memic <adnan.memic27@gmail.com>
2026-09-21 17:17:06 +0200

    refactor(2025/day02): move code block into the try block

--- 2025/day02/Main.java
+++ 2025/day02/Main.java
@@ -5,19 +5,17 @@ public class Main {
 
     public static void main(String[] args) {
         String path = "resources/input.txt";
-        String[] idRanges = null; 
-
         FileHandler file = new FileHandler(path);
+
         try {
-            idRanges = file.readFile();
+            String[] idRanges = file.readFile();
+            Id id = new Id(idRanges);
+            List<String> invalidIds = id.invalidIds();
+            long sum = id.sumOfInvalidIds(invalidIds);
+            System.out.println(sum);
+
         } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.err.println("Error: " + e.getMessage());
         }
-
-        Id id = new Id(idRanges);
-        List<String> invalidIds = id.invalidIds();
-        long sum = id.sumOfInvalidIds(invalidIds);
-
-        System.out.println(sum);
     }
 }
