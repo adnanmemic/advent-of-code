c5e175183dc6e26f62c9694e4b7a4475c6459c2e
Adnan Memic <adnan.memic27@gmail.com>
2026-09-21 13:39:28 +0200

    feat(2025/day02): add filehandler

---
+++ 2025/day02/FileHandler.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private String path;
+    private String[] idRanges;
+
+    public FileHandler (String path) {
+        this.path = path;
+    }
+
+    public String[] readFile () throws IOException{
+        try (BufferedReader file = new BufferedReader(new FileReader(path))) {
+            String content = file.readAllAsString();
+            this.idRanges = content.split(",");
+        }
+
+        return this.idRanges;
+    }
+    
+}
